503a585bd99a33e189b305265b354b0e95ba9eb3
Neal Morton <mortonne@gmail.com>
2022-06-01 08:20:28 -0500

    refactor: remove project flag

--- bin/bender_run_gvt.sh
+++ bin/bender_run_gvt.sh
@@ -3,7 +3,7 @@
 # Run group volume threshold analysis on permutation test results.
 
 if [[ $# -lt 2 ]]; then
-    echo "Usage: bender_run_gvt.sh [-m mask] [-i interp] [-a alpha] [-p nperm] [-A project] filepath subjects"
+    echo "Usage: bender_run_gvt.sh [-m mask] [-i interp] [-a alpha] [-p nperm] filepath subjects"
     exit 1
 fi
 
@@ -12,8 +12,7 @@ interp=BSpline
 mask=${STUDYDIR}/gptemplate/highres_brain_all/gp_template_mni_affine_mask.nii.gz
 n_subj_perm=100
 dry_run=false
-project=ANTS
-while getopts ":a:i:m:p:A:d" opt; do
+while getopts ":a:i:m:p:d" opt; do
     case $opt in
     a)
         alpha=$OPTARG
@@ -26,9 +25,6 @@ while getopts ":a:i:m:p:A:d" opt; do
         ;;
     p)
         n_subj_perm=$OPTARG
-        ;;
-    A)
-        project=$OPTARG
         ;;
     d)
         dry_run=true
@@ -71,14 +67,14 @@ fi
 if [[ $dry_run = true ]]; then
     echo "bender_perm2mni.sh -m ${mask} -i ${interp} {} ${filepath}"
 else
-    jobid1=$(slaunch -J gvt_perm2mni "bender_perm2mni.sh -m $mask -i $interp {} $filepath" "$subjects" -N 13 -n 26 -a 12 -r 01:00:00 -A "$project" | tee -a "$log_file" | getjid)
+    jobid1=$(slaunch -J gvt_perm2mni "bender_perm2mni.sh -m $mask -i $interp {} $filepath" "$subjects" -N 5 -n 30 -a 40 -r 01:00:00 | tee -a "$log_file" | getjid)
 fi
 
 # prep gvt directory, split mask for creating slice file datasets
 if [[ $dry_run = true ]]; then
     echo "bender_prep_gvt.sh -m $mask $filepath $subjects"
 else
-    jobid2=$(ezlaunch -J gvt_prep "bender_prep_gvt.sh -m $mask $filepath $subjects" -N 1 -n 1 -r 00:05:00 -A "$project" -d "$jobid1" | tee -a "$log_file" | getjid)
+    jobid2=$(ezlaunch -J gvt_prep "bender_prep_gvt.sh -m $mask $filepath $subjects" -N 1 -n 1 -r 00:05:00 -d "$jobid1" | tee -a "$log_file" | getjid)
 fi
 
 # merge slice files into datasets with all subjects and all permutations
@@ -87,7 +83,7 @@ bender_prep_gvt_commands.sh "$filepath" "$subjects" > "$prep_file"
 if [[ $dry_run = true ]]; then
     echo "ezlaunch -s $prep_file"
 else
-    jobid3=$(ezlaunch -J gvt_merge -s "$prep_file" -N 1 -n 24 -r 00:30:00 -A "$project" -d "$jobid2" | tee -a "$log_file" | getjid)
+    jobid3=$(ezlaunch -J gvt_merge -s "$prep_file" -N 1 -n 24 -r 00:30:00 -d "$jobid2" | tee -a "$log_file" | getjid)
 fi
 
 # run gvt on each slice
@@ -101,12 +97,12 @@ bender_gvt_commands.sh "$perm_dir" -a "$alpha" > "$run_file"
 if [[ $dry_run = true ]]; then
     echo "ezlaunch -s $run_file"
 else
-    jobid4=$(ezlaunch -J gvt_perm -s "$run_file" -N 10 -n 10 -r 03:00:00 -A "$project" -d "$jobid3" | tee -a "$log_file" | getjid)
+    jobid4=$(ezlaunch -J gvt_perm -s "$run_file" -N 10 -n 10 -r 03:00:00 -d "$jobid3" | tee -a "$log_file" | getjid)
 fi
 
 # merge slices, calculate thresholded image, backup main results
 if [[ $dry_run = true ]]; then
     echo "bender_post_gvt.sh $filepath"
 else
-    ezlaunch -J gvt_post "bender_post_gvt.sh $filepath" -N 1 -n 1 -r 00:10:00 -A "$project" -d "$jobid4" | tee -a "$log_file"
+    ezlaunch -J gvt_post "bender_post_gvt.sh $filepath" -N 1 -n 1 -r 00:10:00 -d "$jobid4" | tee -a "$log_file"
 fi
