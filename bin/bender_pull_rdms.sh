ce65b45f1f1aa0f4bacc17aa669e878ab6908403
Neal Morton <mortonne@gmail.com>
2022-07-13 14:06:43 -0500

    feat: add other searchlights to sync list

--- bin/bender_pull_rdms.sh
+++ bin/bender_pull_rdms.sh
@@ -11,7 +11,12 @@ rsync -azvu "$src" "$dest" \
     --include="batch/glm/" \
     --include="batch/glm/study_stim2/" \
     --include="batch/glm/study_stim2/rdm/" \
+    --include="batch/glm/study_stim2/rdm/study_wiki_w2v_fix_cont_a_bc_sme_*_dil1c/" \
     --include="batch/glm/study_stim2/rdm/study_wiki_w2v_fix_cont_ac_bx_sme_*_dil1c/" \
+    --include="batch/glm/study_stim2/react/" \
+    --include="batch/glm/study_stim2/react/cat_react_item2_*_dil1c/" \
+    --include="batch/glm/study_stim2/react/item_suppress_gvt_*_dil1c/" \
+    --include="batch/glm/study_stim2/react/cat_react_item_sme2_*_dil1c/" \
     --include="bender_??_rdm.txt" \
     --prune-empty-dirs \
     --exclude="*" \
